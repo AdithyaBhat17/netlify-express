fdcf0638ba6be3c74f400d68a46711bdc0884933
Adithya NR <adithyabhat17.ab@gmail.com>
2019-12-06 17:54:19 +0530

    Update server.js

--- express/server.js
+++ express/server.js
@@ -16,5 +16,5 @@ app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
-module.exports = app;
+// module.exports = app;
 module.exports.handler = serverless(app);
